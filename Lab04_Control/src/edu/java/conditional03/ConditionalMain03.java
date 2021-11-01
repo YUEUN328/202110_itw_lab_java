a2e0394e9b50667271ef2e11db6997b214e78293
YUEUN328 <93204370+YUEUN328@users.noreply.github.com>
2021-11-01 23:45:26 +0900

    2021-11-01

---
+++ Lab04_Control/src/edu/java/conditional03/ConditionalMain03.java
@@ -0,0 +1,34 @@
+package edu.java.conditional03;
+
+import java.util.Scanner;
+
+public class ConditionalMain03 {
+
+	public static void main(String[] args) {
+		// 시험 합격/불합격 여부 출력
+		// 시험 과목(Java, SQL, JSP)의 점수를 정수로 입력받고 저장
+		// 모든 과목의 점수가 40점 이상이고, 평균이 60점 이상이면 합격
+		// 그렇지 않으면 불합격을 출력
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Java, SQL, JSP 점수 입력!");
+		int java = sc.nextInt();
+		int sql = sc.nextInt();
+		int jsp = sc.nextInt();
+		double mean = (java + sql + jsp) / 3.0;  // 세 과목의 평균 계산 
+		
+		sc.close(); // 입력 도구 닫기 
+		
+		// 합격/불합격 여부 판단
+		if ((java >= 40) && (sql >=40) && (jsp >=40) && (mean >= 60)) {
+			// 모든 과목의 점수가 40점 이상이고, 평균이 60점 이상이면 합격
+			System.out.println("합격");
+		} else {
+			// 그렇지 않으면 불합격
+			System.out.println("불합격");
+		}
+		
+	}
+
+}
